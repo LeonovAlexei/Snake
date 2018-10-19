0cbac412193e4fdbc2d2f78aa3c81ad2a4c620ff
AlexLeonov <baksleo@gmail.com>
2018-10-19 21:53:57 +0300

    les5

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,20 +10,48 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-                p1.x = 1;
-                p1.y = 2;
-                p1.sym = '*';
-                
-                p1.Draw();
-            Point p2 = new Point();
-                p2.x = 3;
-                p2.y = 5;
-                p2.sym = '#';
-            
-                p2.Draw();
-                Console.ReadLine();
+          //  Point p1 = new Point(1,2,'*');
+                               
+             //   p1.Draw();
+           // Point p2 = new Point(4,5,'#');
+                        
+               // p2.Draw();
+
+            Point p1 = new Point(1, 3, '*');
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            Point p2 = new Point(4, 5, '#');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1 = p2. p1.x = " + p1.x + "p1.y = " + p1.y + ", p2.x = " + p2.x + ", p2.y = " + p2.y);
+
+            p1 = new Point(1, 1, '*');
+            //Update(p1);
+            Console.WriteLine("Call Update. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            Console.ReadLine();
         }
+        public static void Func1(int value)
+        {
+
+        }
+        public static void Func2(int value)
+        {
+            value++;
+        }
+        public static void Func3(int x)
+        {
+            x++;
+        }
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x += dx;
+            p.y += dy;
+        }
+        
+        
 
         
     }
