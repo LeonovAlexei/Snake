db42254708bcaf6ad5b4ac4c4df68c05293d61aa
AlexLeonov <baksleo@gmail.com>
2018-10-19 22:13:33 +0300

    l6

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,57 +10,81 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            //  Point p1 = new Point(1,2,'*');
+            Point p1 = new Point(1, 2, '*');
 
-            //   p1.Draw();
-            // Point p2 = new Point(4,5,'#');
-
-            // p2.Draw();
-            int x = 1;
-            Func1(x);
-            Console.WriteLine("Func1.x = " + x);
-            x = 1;
-            Func2(x);
-            Console.WriteLine("Func2.x = " + x);
-            x = 1;
-            Func3(x);
-            Console.WriteLine("Func3.x = " + x);
-            Point p1 = new Point(1, 3, '*');
-            Move(p1, 10, 10);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
-
+            p1.Draw();
             Point p2 = new Point(4, 5, '#');
-            p1 = p2;
-            p2.x = 8;
-            p2.y = 8;
-            Console.WriteLine("p1 = p2. p1.x = " + p1.x + "p1.y = " + p1.y + ", p2.x = " + p2.x + ", p2.y = " + p2.y);
 
-            p1 = new Point(1, 1, '*');
-            //Update(p1);
-            Console.WriteLine("Call Update. p1.x = " + p1.x + ", p1.y = " + p1.y);
+            p2.Draw();
+            Point p3 = new Point(8, 10, '@');
+            Point p4 = new Point(11, 12, '$');
+            Point p5 = new Point(13, 13, '%');
+            Point p6 = new Point(44, 15, '^');
+            
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            pList.Add(p5);
+            pList.Add(p6);
+            foreach (var item in pList)
 
+            {
+                item.Draw();
+            }
             Console.ReadLine();
-        }
-        public static void Func1(int value)
-        {
+
+            #region lesson5
+
+
+            //    int x = 1;
+            //    Func1(x);
+            //    Console.WriteLine("Func1.x = " + x);
+            //    x = 1;
+            //    Func2(x);
+            //    Console.WriteLine("Func2.x = " + x);
+            //    x = 1;
+            //    Func3(x);
+            //    Console.WriteLine("Func3.x = " + x);
+            //    Point p1 = new Point(1, 3, '*');
+            //    Move(p1, 10, 10);
+            //    Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            //    Point p2 = new Point(4, 5, '#');
+            //    p1 = p2;
+            //    p2.x = 8;
+            //    p2.y = 8;
+            //    Console.WriteLine("p1 = p2. p1.x = " + p1.x + "p1.y = " + p1.y + ", p2.x = " + p2.x + ", p2.y = " + p2.y);
+
+            //    p1 = new Point(1, 1, '*');
+            //    //Update(p1);
+            //    Console.WriteLine("Call Update. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            //    Console.ReadLine();
+            //}
+            //public static void Func1(int value)
+            //{
+
+            //}
+            //public static void Func2(int value)
+            //{
+            //    value++;
+            //}
+            //public static void Func3(int x)
+            //{
+            //    x++;
+            //}
+            //public static void Move(Point p, int dx, int dy)
+            //{
+            //    p.x += dx;
+            //    p.y += dy;
+            //}
+
+            #endregion
+
 
         }
-        public static void Func2(int value)
-        {
-            value++;
-        }
-        public static void Func3(int x)
-        {
-            x++;
-        }
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x += dx;
-            p.y += dy;
-        }
-        
-        
 
-        
     }
 }
